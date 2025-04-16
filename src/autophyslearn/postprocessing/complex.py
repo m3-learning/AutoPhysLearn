e88f9d001d8ab255744cbe2853aede5866c044ae
Julian Goddy <jg3837@drexel.edu>
2025-04-16 14:25:05 -0400

    rename stuff

--- src/autophyslearn/postprocessing/complex.py
+++ src/autophyslearn/postprocessing/complex.py
@@ -8,7 +8,7 @@ class ComplexPostProcessor:
         dataset (object): The dataset containing the scalers used for normalizing the real and imaginary parts.
     """
 
-    def __init__(self, dataset):
+    def __init__(self, dataset, device="cuda:0"):
         """
         Initializes the ComplexPostProcessor with the given dataset.
 
@@ -16,7 +16,7 @@ class ComplexPostProcessor:
             dataset (object): The dataset object containing scalers for real and imaginary components.
         """
         self.dataset = dataset
-
+        self.device = device
     def compute(self, fits):
         """
         Processes the complex fits by extracting, scaling, and stacking the real and imaginary components.
@@ -32,15 +32,15 @@ class ComplexPostProcessor:
         real = torch.real(fits)
 
         # Scale the real part using the dataset's real scaler, moving data to the GPU (cuda)
-        real_scaled = (real - torch.tensor(self.dataset.raw_data_scaler.real_scaler.mean).cuda()) / \
-                      torch.tensor(self.dataset.raw_data_scaler.real_scaler.std).cuda()
+        real_scaled = (real - torch.tensor(self.dataset.raw_data_scaler.real_scaler.mean).to(self.device)) / \
+                      torch.tensor(self.dataset.raw_data_scaler.real_scaler.std).to(self.device)
 
         # Extract the imaginary part of the complex tensor
         imag = torch.imag(fits)
 
         # Scale the imaginary part using the dataset's imaginary scaler, moving data to the GPU (cuda)
-        imag_scaled = (imag - torch.tensor(self.dataset.raw_data_scaler.imag_scaler.mean).cuda()) / \
-                      torch.tensor(self.dataset.raw_data_scaler.imag_scaler.std).cuda()
+        imag_scaled = (imag - torch.tensor(self.dataset.raw_data_scaler.imag_scaler.mean).to(self.device)) / \
+                      torch.tensor(self.dataset.raw_data_scaler.imag_scaler.std).to(self.device)
 
         # Stack the scaled real and imaginary components along a new dimension
         out = torch.stack((real_scaled, imag_scaled), dim=2)
